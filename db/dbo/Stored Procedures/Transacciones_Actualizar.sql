af973e615ddc9e3a616b4c1f392ec9e4727a8fb2
MarCarrera <marcarrera00@gmail.com>
2022-09-19 19:56:31 -0500

    Proyecto Presupuesto ASP.NET Core

---
+++ db/dbo/Stored Procedures/Transacciones_Actualizar.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE Transacciones_Actualizar
+@Id                     int,
+@FechaTransaccion       date,
+@Monto					decimal(18,2),
+@MontoAnterior			decimal(18,2),
+@CategoriaId			int,
+@CuentaId				int,
+@CuentaAnteriorId		int,
+@Nota					nvarchar(1000) = NULL
+AS
+BEGIN
+--REVERTIR TRANSACCION ANTERIOR
+UPDATE Cuentas SET Balance -= @MontoAnterior
+WHERE Id = @CuentaAnteriorId;
+
+--TEALIZAR NUEVA TRANSACCION
+UPDATE Cuentas SET Balance += @Monto
+WHERE Id = @CuentaId
+
+UPDATE Transacciones SET Monto =ABS(@Monto),
+FechaTransaccion = @FechaTransaccion,
+CategoriaId = @CategoriaId, CuentaId = @CuentaId,
+Nota = @Nota
+WHERE Id = @Id
+
+END
