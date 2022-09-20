72d1f7bcdd5c9daab792641f7050b3a48faf9452
MarCarrera <marcarrera00@gmail.com>
2022-09-19 20:42:27 -0500

    Proyecto Presupuesto

--- db/dbo/Tables/Cuentas.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Cuentas] (
-    [Id]           INT             IDENTITY (1, 1) NOT NULL,
-    [Nombre]       NVARCHAR (50)   NOT NULL,
-    [TipoCuentaId] INT             NOT NULL,
-    [Balance]      DECIMAL (18, 2) NOT NULL,
-    [Descripcion]  NVARCHAR (1000) NULL,
-    CONSTRAINT [PK_Cuentas] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
